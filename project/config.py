0a6ffc38ee05f48375e4c93bf8be2922803b6165
csakhan <csakhan@gmail.com>
2020-09-26 10:44:29 +0100

    added entrypoints

--- project/config.py
+++ project/config.py
@@ -1,7 +1,10 @@
 
+import os
+
 class BaseConfig:
     TESTING = False
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevelopmentConfig(BaseConfig):
     pass
@@ -9,6 +12,7 @@ class DevelopmentConfig(BaseConfig):
 
 class TestingConfig(BaseConfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
